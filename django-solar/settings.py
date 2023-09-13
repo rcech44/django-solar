688f402aa7e643993de9d0466e3fb4e68c7bbe70
Radomir Cech <rcech44@gmail.com>
2023-09-13 21:25:38 +0200

    Removed secret

--- django-solar/settings.py
+++ django-solar/settings.py
@@ -1,14 +1,24 @@
 from pathlib import Path
+import environ
+import os
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-_t1^^f8hmshsq*vfu*a0(qj6t^6zrppktjwfdj2y9ui@-ucl$x'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
