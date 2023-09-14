592dce85b8b2db31e5780e4023d3bab18375375f
Radomir Cech <rcech44@gmail.com>
2023-09-14 16:46:30 +0200

    Updates to manager

--- api/urls.py
+++ api/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('session/', views.session_view, name='api-session'),
     path('getCompanyInfo/', views.company_info, name='api-company-info'),
     path('whoami/', views.whoami_view, name='api-whoami'),
+    path('infoMe/', views.current_user_info_view, name='api-infome'),
+    path('compare/', views.compare_view, name='api-compare'),
 ]
--- api/views.py
+++ api/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.decorators.http import require_POST
+from .models import Offer, User
+from django.forms.models import model_to_dict
 
 
 @require_POST
@@ -23,14 +25,12 @@ def login_view(request):
     login(request, user)
     return JsonResponse({'detail': 'Successfully logged in.'})
 
-
 def logout_view(request):
     if not request.user.is_authenticated:
         return JsonResponse({'detail': 'You\'re not logged in.'}, status=400)
 
     logout(request)
     return JsonResponse({'detail': 'Successfully logged out.'})
-
 
 @ensure_csrf_cookie
 def session_view(request):
@@ -46,9 +46,34 @@ def company_info(request):
 
     return JsonResponse({'company_long_name': request.user.company_name})
 
+@ensure_csrf_cookie
+def current_user_info_view(request):
+    if not request.user.is_authenticated:
+        return JsonResponse({'isAuthenticated': False})
+    
+    user_id = request.user.id
+    user = User.objects.get(id=user_id)
+    user_dict = model_to_dict( user )
+    result = {
+        'id': user_dict['id'],
+        'username': user_dict['username'],
+        'first_name': user_dict['first_name'],
+        'last_name': user_dict['last_name'],
+        'email': user_dict['email'],
+        'is_companystaff': user_dict['is_companystaff'],
+        'company_short_name': user_dict['company_short_name'],
+        'company_name': user_dict['company_name'],
+        'company_image': user_dict['company_image']
+    }
+
+    return JsonResponse({'user': result})
 
 def whoami_view(request):
     if not request.user.is_authenticated:
         return JsonResponse({'isAuthenticated': False})
 
     return JsonResponse({'username': request.user.username})
+
+def compare_view(request):
+    all_offers = list(Offer.objects.values())
+    return JsonResponse({'data': all_offers})
